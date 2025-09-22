b0fcd0548903ede9769a57d6157bad42f96059a2
vishalbuilds <vibisingh@gmail.com>
2025-09-22 10:22:45 +0530

    Remove secret key files and update secrets_client.py
    Deleted access_secret.key and refresh_secret.key files and migrated secret keys directly into secrets_client.py. Updated secrets_client.py to use JWTHandler for token verification, improving key management and token handling.

--- src/utils/secrets_client.py
+++ src/utils/secrets_client.py
@@ -1,5 +1,25 @@
-import secrets
+# import secrets
 
-# Generate a cryptographically secure random key
-secret_key = secrets.token_urlsafe(32)  # 32 bytes ≈ 43 characters
-print(secret_key)
+# # Generate a cryptographically secure random key
+# secret_key = secrets.token_urlsafe(32)  # 32 bytes ≈ 43 characters
+# print(secret_key)
+
+
+
+
+
+from jwt_handler import JWTHandler
+
+
+
+ACCESS_SECRET_KEY = "xhS4oXm2YAmy77Fi0nHfJDlSLnNrCtRtr0dTZ4ZnjkM"
+REFRESH_SECRET_KEY="ri3PML6FuYlIGeOGK5DL1FsIexoYOSuLoJlbh_B6POI"
+token_obj=JWTHandler(REFRESH_SECRET_KEY,ACCESS_SECRET_KEY)
+
+
+
+a=token_obj.verify_access_token("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMWY4ZWNhYmEtZmE5OS00YjViLWIyMjctYWRlMDUwZTQxYzU4IiwidXNlcm5hbWUiOiJ2aXNoYWxfc2luZ2giLCJqdGkiOiI2MGI0YTczNC1jYzE1LTQzNmYtOTRkMS02NWIxNDA0YTZiZmIiLCJleHAiOjE3NTg0NjU0MTJ9.xPGHgscFu9W1JPJNV00Az0rBUSIptOtjaTOLmbrxNSg",        
+                        refresh=True        
+                                )
+
+print(a)
